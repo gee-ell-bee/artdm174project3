e7bff4028d581f842962f221b2136fae026c778d
Grey Larrain Bratt <greybratt@outlook.com>
2022-04-07 16:00:34 -0700

    added text tracks for redubbed ver

---
+++ js/video_helpers.js
@@ -0,0 +1,67 @@
+/** clipeo_helpers.js
+ *  
+ * Helper functions for controlling the clipeo element.
+ * by Troy Bennett 7-2010
+ *
+ */
+
+
+ function selectMedia(e, clip) 
+ {
+     clip.src = e.target.value;
+     clip.load();
+     playVideo(clip);
+ }
+
+ function playMedia(clip) 
+ {
+     clip.play();
+ }
+ 
+ function pauseVideo(clip) 
+ {
+     clip.pause();
+ }
+
+ function stopVideo(clip) {
+     clip.pause();
+     clip.currentTime = 0;
+     clip.play();
+ }
+ function seekVideo(clip, position) 
+ {
+     if(!position) position = 0;
+     clip.currentTime = position;
+     clip.play();
+ }
+
+ function muteVideo(clip) 
+ {
+     clip.muted = true;
+     if(document.getElementById("unmute").disabled = true) {
+         document.getElementById("unmute").disabled = false;
+     }
+     document.getElementById("mute").disabled = true;
+ }
+
+ function unmuteVideo(clip) 
+ {
+     clip.muted = false;
+     document.getElementById("mute").disabled = false;
+     document.getElementById("unmute").disabled = true;
+ }
+
+ function onFinished(clip) 
+ {
+     clip.currentTime = 0;
+ }
+
+ // rate can be a positive integer
+ // .5 is half speed, 1 is normal speed,
+ // 2 is double speed, etc.
+ //Only Safari supports negative values (backwards)
+ function playRate(clip, rate) 
+ {
+     clip.playbackRate = rate;
+ }
+
