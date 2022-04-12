54cd34e2da77dd2b2e15204f52bd0aab28b8c836
Grey Larrain Bratt <greybratt@outlook.com>
2022-04-12 15:05:24 -0700

    initial working captions

--- js/main.js
+++ js/main.js
@@ -14,61 +14,12 @@ const vid = document.querySelector('#vid');
 let docFrag = new DocumentFragment();
 let langSection = document.querySelector('#caps');
 //differentiate between each section
-const langList = ['selected', 'other'];
+const langList = ['en', 'zh'];
 const redubChapterTrack = document.querySelector('#redubChapters');
 
 
 
-// function to make each language section
-langList.forEach(function(lang) {
-    let section = document.createElement('section');
-    section.classList.add('lang');
-    section.setAttribute('id', lang);
-    docFrag.appendChild(section);
-    console.log({vid});
-});
 
-
-/* adapted from Rex Barkdoll's "HTML5 Video with Chapters"
-/ original: http://thenewcode.com/977/Create-Interactive-HTML5-Video-with-WebVTT-Chapters
-/ updated source also referenced: https://codepen.io/rexbarkdoll/pen/XWMNwJM */
-// chapters links
-function displayChapters(chapElem){
-	if (chapElem){
-        const textTrack = chapElem.track;
-		if (textTrack.kind === "chapters"){
-			textTrack.mode = 'hidden';
-			for (var i = 0; i < textTrack.cues.length; ++i) {
-                let chapterList = document.querySelector('#chapterOl');
-				let cue = textTrack.cues[i];
-				let chapterName = cue.text;
-				let start = cue.startTime;
-				const newChapter = document.createElement("li");
-				newChapter.setAttribute('id', start);
-				var chapterTitle = document.createTextNode(cue.text);
-				newChapter.appendChild(chapterTitle);
-				chapterList.appendChild(newChapter);
-				newChapter.addEventListener("click",
-				function() {
-					vid.currentTime = this.id;
-                    vid.play();
-				},false);
-			}
-		textTrack.addEventListener("cuechange",
-			function() {
-				var currentLocation = this.activeCues[0].startTime;
-				if (chapter = document.getElementById(currentLocation)) {
-					var locations = [].slice.call(document.querySelectorAll("#chapters li"));
-					for (var i = 0; i < locations.length; ++i) { 
-						locations[i].classList.remove("current");
-					}
-					chapter.classList.add("current");
-					locationList.style.top = "-"+chapter.parentNode.offsetTop+"px";
-				}
-			},false);
-		}
-	}
-}
 
 /* by Troy Bennett 7-2010 (updated 12-2021) */
 import { cueTimer } from "./modules/cuepoints.js";
@@ -77,9 +28,21 @@ document.addEventListener("DOMContentLoaded", init);
 
 /* //end by Troy Bennett */
 function init() {
+
+    // function to make each language section
+        langList.forEach(function(lang) {
+            let section = document.createElement('section');
+            section.classList.add('lang');
+            section.setAttribute('id', lang);
+            docFrag.appendChild(section);
+            console.log({vid});
+        });
     
+    let engLang = document.getElementById('en');
+    let chiLang = document.getElementById('zh');
+
     // add chapter links
-    displayChapters(redubChapterTrack);
+    displayCaps(redubChapterTrack);
 
     // show the language HTML sections
     vid.addEventListener('play', () => {
@@ -87,7 +50,6 @@ function init() {
             langSection.classList.add('show');
             //append langSection to document fragment
             langSection.appendChild(docFrag);
-        
         };
     });
     
@@ -104,6 +66,21 @@ function init() {
 
     //shortcut variables
     const selectList = document.querySelector("#video_select");
+    const selectOpts = selectList.querySelectorAll('option');
+    console.log(selectOpts);
+    selectOpts.forEach(source => {
+        let srcType = source.value;
+        /*if(srcType = type) {
+        };*/
+        let supported = vid.canPlayType(srcType);
+        console.log(srcType);
+        if(supported === "") {
+            source.hidden = "true";
+        }
+
+    }
+
+    );
 
     // make the select list control what video format to play
     selectList.addEventListener("change", (e) => {
@@ -111,10 +88,90 @@ function init() {
     });
 }
 
-    //vid event listener to pause when seeking
-    vid.addEventListener('seeking', (e) => {
-        vid.pause();
-    });
+
+/* adapted from Rex Barkdoll's "HTML5 Video with Chapters"
+/ original: http://thenewcode.com/977/Create-Interactive-HTML5-Video-with-WebVTT-Chapters
+/ updated source also referenced: https://codepen.io/rexbarkdoll/pen/XWMNwJM */
+// chapters links
+function displayCaps(chapElem){
+	if (chapElem){
+        const textTrack = chapElem.track;
+		if (textTrack.kind === "chapters"){
+			textTrack.mode = 'hidden';
+			for (var i = 0; i < textTrack.cues.length; ++i) {
+                let chapterList = document.querySelector('#chapterOl');
+				let cue = textTrack.cues[i];
+				//let chapterName = cue.text;
+				let start = cue.startTime;
+				const newChapter = document.createElement("li");
+				newChapter.setAttribute('id', start);
+				var chapterTitle = document.createTextNode(cue.text);
+				newChapter.appendChild(chapterTitle);
+				chapterList.appendChild(newChapter);
+				newChapter.addEventListener("click",
+				function() {
+					vid.currentTime = this.id;
+                    vid.play();
+				},false);
+			}
+            
+		current.track.addEventListener("cuechange",
+			function(e) {
+                let chapterCue = e.target.activeCues[0].text;
+                const langElem = document.getElementById('selected');
+                langElem.textContent = chapterCue;
+				//let chapterCue = e.target.activeCues[0].startTime;
+                
+				/*if (chapterCue = document.getElementById(currentLocation)) {
+					var locations = [].slice.call(document.querySelectorAll("#chapters li"));
+					for (var i = 0; i < locations.length; ++i) { 
+						locations[i].classList.remove("current");
+					}
+					chapter.classList.add("current");
+					locationList.style.top = "-"+chapter.parentNode.offsetTop+"px";
+				}*/
+			},false);
+		}
+	}
+}
+
+function displayCapts(vidSrc, trackLang){
+
+	if (trackLang){
+        const textTrack = trackLang.track;
+		textTrack.mode = 'hidden';
+			for (var i = 0; i < textTrack.cues.length; ++i) {
+                let chapterList = document.querySelector('#chapterOl');
+				let cue = textTrack.cues[i];
+				let chapterName = cue.text;
+				let start = cue.startTime;
+				const newChapter = document.createElement("li");
+				newChapter.setAttribute('id', start);
+				var chapterTitle = document.createTextNode(cue.text);
+				newChapter.appendChild(chapterTitle);
+				chapterList.appendChild(newChapter);
+				newChapter.addEventListener("click",
+				function() {
+					vid.currentTime = this.id;
+                    vid.play();
+				},false);
+			}
+            /*
+		textTrack.addEventListener("cuechange",
+			function() {
+				var currentLocation = this.activeCues[0].startTime;
+				if (chapter = document.getElementById(currentLocation)) {
+					var locations = [].slice.call(document.querySelectorAll("#chapters li"));
+					for (var i = 0; i < locations.length; ++i) { 
+						locations[i].classList.remove("current");
+					}
+					chapter.classList.add("current");
+					locationList.style.top = "-"+chapter.parentNode.offsetTop+"px";
+				}
+			},false); 
+		}*/
+	}
+}
 
 function subtitleAttention() { // pop-up to let user know they can change the sub/captions language from English to Chinese
     let pop = document.querySelector(".pop");
